8165175cebe8909ddbf67cf69dace8721a6546b5
zhaoweifan <1944066736@qq.com>
2019-01-14 12:02:34 +0800

    美剧天堂

--- movie/pipelines.py
+++ movie/pipelines.py
@@ -5,9 +5,21 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+# 管道： 数据清洗、去重
+# 持久化： 写入txt csv   写入数据库
+
+# scrapy 框架将爬取spider模块和处理层分离开，使得程序更容易扩展
+# spider yield生成的item会交给pipline处理。如果爬取速度跟处理速度不一致的话，scrapy框架会自动调度。
+# spirder yield相当于生产消费模型中的生产者，pipeline相当于消费者。如果爬取解析速度快于pipeline，那么还没来得及处理的item会进入到队列当中（item_chain）。
 
 class MoviePipeline(object):
     def process_item(self, item, spider):
-        with open("my_meijutt.text", 'a') as fp:
-            fp.write(item['name'].encode('utf-8') + '\n')
+        # 爬虫部分在for循环中yield item,所以process_item 方法会重复执行
+        # open(mode='a')追加模式，如果w模式的话会覆盖掉上次写的信息。
+        with open('my_meijutt.text' , 'a',encoding='utf-8') as fp:# 模式为a模式，不会被覆盖
+            fp.write(str(item['name']) + '\n') # item 的name
         return item
+
+
+
+
--- movie/spiders/meiju.py
+++ movie/spiders/meiju.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from movie.items import MovieItem
+from movie.items import MovieItem   # 这是个路径
 
 class MeijuSpider(scrapy.Spider):
     name = 'meiju'    # 爬虫名字 一个项目可能有多个爬虫，并且每个爬虫有优先级、并发等设置
@@ -11,8 +11,16 @@ class MeijuSpider(scrapy.Spider):
         # print(response.status_code,response.content,response.text)
         # 非框架写法 dom = lxml.etree.HTML(response.text) ; dom.xpath('')
         # scrpay框架正则写法 Selector(response.text).xpath('').extract()
-        movies = response.xpath('/html/body/div[3]/div[3]/div[1]/ul/li')
-        for each_movie in movies:
+
+        movie_list = response.xpath('/html/body/div[3]/div[3]/div[1]/ul/li')
+        for movie_li in movie_list:
+
+            # 解析后跟.extract() 下标[0]   .extract   =  .extract_first()  二者选其一
+            # xpath().返回[Selector(),Selector()]     xpath().extract()返回['剧集名','剧集名2'xpath]          xpath().extract_first()  返回'剧集名1'
+            # . 表示在子标签基础上继续解析
+            name = movie_li.xpath('./h5/a/@title()').extract()[0]
             item = MovieItem()
-            item['name'] = each_movie.xpath('./h5/a/@title()').extract()[0]
-            yield item
+            # item.name = name  第二种方式
+            item['name'] = name
+
+            yield item         # 相当于return
